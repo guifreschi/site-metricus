878b624c80cc85338a5d80f3998074f6e9fa8c4b
Guilherme Freschi <guilhermefreschix@gmail.com>
2024-12-27 18:12:10 -0300

    Initial adjustments to simple conversions logic

--- script.js
+++ script.js
@@ -3,6 +3,15 @@ const toggleButton = document.getElementById('light-mode-toggle')
 const switchButton = document.querySelector('#switch')
 const circle = document.querySelector('#circle')
 
+const select = document.querySelector('#select')
+const back = document.querySelector('#back')
+const selectSimple = document.querySelectorAll('#select #simple ul li')
+const selectComplex = document.querySelectorAll('#select #complex ul li')
+const unitInfo = document.querySelector('#calculator #unit-info h2')
+const calculator = document.querySelector('#calculator')
+const calculateButton = document.querySelector('#calculate-operation')
+const result = document.querySelector('#result')
+
 if (localStorage.getItem('light-mode') === 'enabled') {
   body.classList.add('light-mode')
   toggleButton.src = 'assets/moon.svg'
@@ -34,4 +43,22 @@ toggleButton.addEventListener('click', () => {
 
 switchButton.onclick = () => {
   circle.classList.toggle('circle-animation')
+}
+
+selectSimple.forEach(item => {
+  item.onclick = () => {
+    unitInfo.textContent = item.id
+    calculator.classList.remove('display-none')
+    select.classList.add('display-none')
+  }
+})
+
+back.onclick = () => {
+  calculator.classList.add('display-none')
+  select.classList.remove('display-none')
+}
+
+calculateButton.onclick = (e) => {
+  e.preventDefault()
+  result.classList.remove('display-none')
 }
