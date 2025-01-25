a44e4924d8691dcdde777eb8455942f579c305db
Guilherme Freschi <guilhermefreschix@gmail.com>
2025-01-25 20:16:24 -0300

    Changing baseURL

--- src/main/server/static/js/config.js
+++ src/main/server/static/js/config.js
@@ -1,2 +1,2 @@
-export const baseURL = 'http://127.0.0.1:5000'
-// export const baseURL = 'https://metricus.onrender.com'
+// export const baseURL = 'http://127.0.0.1:5000'
+export const baseURL = 'https://metricus-55e35472726a.herokuapp.com'
